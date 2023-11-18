88b46f7c058b8ea51f100f006cdbb8b892d35975
jorotyn <73930194+jorotyn@users.noreply.github.com>
2023-11-18 18:31:23 +1100

    Finished implementation of the AIActionIdle script
    - Workers in the idle state will wait a random amount of time, then pick a random destination to move to on the navmesh

--- Assets/Scripts/Worker AI/AIActionIdle.cs
+++ Assets/Scripts/Worker AI/AIActionIdle.cs
@@ -1,19 +1,19 @@
 using MoreMountains.Tools;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AIActionIdle : AIAction
 {
     #region Serialized Fields
-    [Header("Idle Movement Settings")]
-    [SerializeField] private float minWaitTime = 2f;
-    [SerializeField] private float maxWaitTime = 5f;
+    [SerializeField] private float wanderRadius;
+    [SerializeField] private float minWanderTimer;
+    [SerializeField] private float maxWanderTimer;
     #endregion
 
     #region Private Fields
     private NavMeshAgent navMeshAgent;
-    private bool isMoving;
+    private float timer;
+    private float wanderTimer;
     #endregion
 
     #region Initialization
@@ -21,79 +21,42 @@ public class AIActionIdle : AIAction
     {
         base.Initialization();
         navMeshAgent = GetComponentInParent<NavMeshAgent>();
+        RandomizeWanderTimer();
     }
     #endregion
 
     #region Public Methods
     public override void PerformAction()
     {
-        if (isMoving)
-        {
-            Debug.Log("Already moving, returning from PerformAction");
-            return;
-        }
+        timer += Time.deltaTime;
 
-        //Debug.Log("Performing Idle Action");
-        //StartCoroutine(IdleRoutine());
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            navMeshAgent.SetDestination(newPos);
+            timer = 0;
+            RandomizeWanderTimer();
+        }
     }
     #endregion
 
-    #region Private Methods
-    private IEnumerator IdleRoutine()
+    #region Utility Methods
+    private static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
     {
-        isMoving = true;
-        Debug.Log("Starting IdleRoutine");
+        Vector3 randDirection = Random.insideUnitSphere * dist;
 
-        // Move to a random destination
-        MoveToRandomDestination();
+        randDirection += origin;
 
-        // Wait until the destination is reached
-        yield return new WaitUntil(() => !navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.1f);
+        NavMeshHit navHit;
 
-        Debug.Log("Destination reached");
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
 
-        // Wait for a random amount of time
-        float waitTime = Random.Range(minWaitTime, maxWaitTime);
-        yield return new WaitForSeconds(waitTime);
-
-        Debug.Log($"Waited for {waitTime} seconds");
-
-        isMoving = false;
+        return navHit.position;
     }
 
-    private void MoveToRandomDestination()
+    private void RandomizeWanderTimer()
     {
-        int maxAttempts = 10; // Maximum number of attempts to find a valid destination
-        int attempts = 0;
-        bool validDestinationFound = false;
-        NavMeshHit hit;
-
-        while (!validDestinationFound && attempts < maxAttempts)
-        {
-            Vector3 randomDirection = Random.insideUnitSphere * navMeshAgent.stoppingDistance;
-            randomDirection += transform.position;
-
-            if (NavMesh.SamplePosition(randomDirection, out hit, navMeshAgent.stoppingDistance, NavMesh.AllAreas))
-            {
-                navMeshAgent.SetDestination(hit.position);
-                Debug.Log($"Moving to new destination: {hit.position}");
-                validDestinationFound = true;
-            }
-            else
-            {
-                Debug.Log("Trying to find a valid destination");
-                attempts++;
-            }
-        }
-
-        if (!validDestinationFound)
-        {
-            Debug.Log("Failed to find a valid destination after maximum attempts");
-            // Handle the case where no valid destination is found. For example:
-            // - Trigger another action
-            // - Move to a default position
-            // - Log an error or warning
-        }
+        wanderTimer = Random.Range(minWanderTimer, maxWanderTimer);
     }
     #endregion
 }
