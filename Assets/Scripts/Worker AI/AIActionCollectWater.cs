d9f1a551d777fdd88538c6c744a334fdcaf7a7cf
jorotyn <73930194+jorotyn@users.noreply.github.com>
2023-11-18 22:42:05 +1100

    Implemented AIActionCollectWater script
    - Updated the AIActionCollectWater class that inherits from AIAction
    - Allows workers to find the nearest Water that is not locked, lock it, and navigate towards it
    - When the worker is within the specified interaction range, it interacts with the Water object and unlocks it
    - Interacting with the Water object removes it from the WaterManager and destroys its game object. It also records its water value
    - The worker then navigates towards to the Plant
    - Once in range, the worker provides the water value to the plant and clears the water from itself
    - If the worker exits the state for any reason while carrying a water, it will keep it in it's inventory

--- Assets/Scripts/Worker AI/AIActionCollectWater.cs
+++ Assets/Scripts/Worker AI/AIActionCollectWater.cs
@@ -1,11 +1,131 @@
 using MoreMountains.Tools;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
 public class AIActionCollectWater : AIAction
 {
+    #region Serialized Fields
+    [SerializeField] private float interactionRange = 1f;
+    #endregion
+
+    #region Private Fields
+    private NavMeshAgent navMeshAgent;
+    private Water nearestWater;
+    private Transform plantTransform;
+    private float waterAmountCollected;
+    private bool isWaterCollected;
+    #endregion
+
+    #region Lifecycle
+    public override void OnEnterState()
+    {
+        base.OnEnterState();
+        nearestWater = null;
+    }
+
+    public override void OnExitState()
+    {
+        base.OnExitState();
+        if (nearestWater != null)
+        {
+            nearestWater.Unlock();
+            nearestWater = null;
+        }
+    }
+    #endregion
+
+    #region Initialization
+    public override void Initialization()
+    {
+        base.Initialization();
+        navMeshAgent = GetComponentInParent<NavMeshAgent>();
+        plantTransform = Plant.Instance.GetPlantLocation();
+    }
+    #endregion
+
     #region Public Methods
     public override void PerformAction()
     {
+        LocateNearestWater();
+        CollectNearestWater();
+        TakeWaterToPlant();
+        DeliverWater();
+    }
+    #endregion
 
+    #region Private Methods
+    private void LocateNearestWater()
+    {
+        if (!isWaterCollected)
+        {
+            if (nearestWater == null)
+            {
+                float nearestWaterDistance = Mathf.Infinity;
+                List<Water> allWater = WaterManager.Instance.GetAllWaterComponents();
+
+                foreach (Water water in allWater)
+                {
+                    if (water != null && !water.IsLocked())
+                    {
+                        float distanceToWater = Vector3.Distance(transform.position, water.transform.position);
+                        if (distanceToWater < nearestWaterDistance)
+                        {
+                            nearestWater = water;
+                            nearestWaterDistance = distanceToWater;
+                        }
+                    }
+                }
+            }
+
+            if (nearestWater != null)
+            {
+                if (nearestWater.GetLockingAgent() == null)
+                {
+                    nearestWater.Lock(this);
+                    navMeshAgent.SetDestination(nearestWater.transform.position);
+                }
+            }
+        }
+    }
+
+    private void CollectNearestWater()
+    {
+        if (!isWaterCollected && nearestWater != null)
+        {
+            float distanceToWater = Vector3.Distance(transform.position, nearestWater.transform.position);
+
+            if (distanceToWater <= interactionRange)
+            {
+                waterAmountCollected = nearestWater.GetWaterAmount();
+                nearestWater.Interact();
+                nearestWater.Unlock();
+                nearestWater = null;
+                isWaterCollected = true;
+            }
+        }
+    }
+
+    private void TakeWaterToPlant()
+    {
+        if (isWaterCollected)
+        {
+            navMeshAgent.SetDestination(plantTransform.position);
+        }
+    }
+
+    private void DeliverWater()
+    {
+        if (isWaterCollected)
+        {
+            float distanceToPlant = Vector3.Distance(transform.position, plantTransform.position);
+            if (distanceToPlant <= interactionRange)
+            {
+                ResourceManagementSystem.Instance.AddWater(waterAmountCollected);
+                isWaterCollected = false;
+                waterAmountCollected = 0f;
+            }
+        }
     }
     #endregion
 }
